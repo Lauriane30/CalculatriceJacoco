b6f19ae06c4c4df3fcf9b266f588d80bccc5c1a5
Lauriane30 <73834419+Lauriane30@users.noreply.github.com>
2021-12-03 17:56:32 +0100

    Update Module1ClassA.java

--- plugins/module1/src/am/tychosonarjacoco/module1/Module1ClassA.java
+++ plugins/module1/src/am/tychosonarjacoco/module1/Module1ClassA.java
@@ -2,6 +2,32 @@ package am.tychosonarjacoco.module1;
 
 public class Module1ClassA implements AllModulesInterface {
 
+	private int x;
+    	private int y;
+    	public Module1ClassA(int a, int b) {
+		x = a;
+		y = b;
+    	}
+    	public int ajouter() {
+		x += y;
+		return x;
+   	}
+
+   	 public int soustraire() {
+		x -= y;
+		return x;
+    	}
+
+    	public int multiplier() {
+		x *= y;
+		return x;
+    	}
+
+    	public int diviser() {
+		x /= y;
+		return x;
+    	}
+	
 	@Override
 	public String getName() {
 		return "1A";
